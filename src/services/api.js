e6d812ddb929e721e63aa361a8f01226a1940e5a
Kitae's Github <aqua0405@gmail.com>
2026-01-09 01:11:02 +0900

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_BASE_URL = "http://localhost:8080/api";
+const API_BASE_URL = "http://114.71.147.30:28080/api";
+// const API_BASE_URL = "http://114.71.147.30:8080/api";
 
 // axios 인스턴스 생성
 const api = axios.create({
